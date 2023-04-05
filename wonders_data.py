71f555e6ab9f5a9b2741bd609950332a33b8b736
Villelligiulia <villelligiulia@gmail.com>
2023-04-05 17:59:19 +0000

    Create a list of dictionaries with the Wonders of Mondern and Ancient World

---
+++ wonders_data.py
@@ -0,0 +1,91 @@
+# A data list of Dictionaries with 7 Wonders of Modern World and 7 Wonders of Ancient (Classic)  World.
+
+data = [
+    # Modern Wonders World
+    {'name': "Chicken Itza",
+     'country' : "Mexico",
+     'year_built': 800,
+     'description' : "a complex of Mayan ruins on Mexico's Yucatan Peninsula."
+    
+    },
+    {'name': "Christ the Redeemer",
+     'country': "Brazil",
+     'year_built': 1922,
+     'description' : "a large statue of Jesus Christ in Rio de Janeiro, Brazil."
+    
+    },
+    {'name': "Colosseum",
+     'country': "Italy",
+     'year_built': 72,
+     'description' : "an ancient amphitheater in Rome, Italy, used for gladiatorial contests and public spectacles."
+
+    },
+    {'name': "Great Wall of China",
+     'country': "China",
+     'year_built': 700,
+     'description' : "a massive wall built across northern China to protect against invasions."
+
+    },
+    {'name': "Machu Pichu",
+     'country': "Peru",
+     'year_built': 1440,
+     'description' : "an Incan citadel and UNESCO World Heritage Site in Peru."
+    
+    },
+    {'name': "Petra",
+     'country': "Jordan",
+     'year_built': -400,
+      'description' : "an ancient city in Jordan known for its stunning rock-cut architecture."
+    
+    },
+    {'name': "Taj Mahal",
+     'country': "India",
+     'year_built': 1631,
+     'description' : "a white marble mausoleum in Agra, India, built by the Mughal emperor Shah Jahan in memory of his wife Mumtaz Mahal."
+    
+    },
+    # Classic Wonder Worlds
+    {'name': "Great Pyramid of Giza",
+     'country': "Egypt",
+     'year_built': -2560, 
+     'description' : "the oldest and largest pyramid in Egypt, built as a tomb for the Pharaoh Khufu."
+    
+    },
+    {'name': "The Statue of Zeus in Olympia",
+     'country': "Greece",
+     'year_built': -500,
+     'description' : "a giant statue of the Greek god Zeus created by the sculptor Phidias."
+    
+    },
+    {'name': "Hanging Gardens of Babilon",
+     'country': "Iraq",
+     'year_built': -600, 
+     'description' : " a terraced garden complex built in Mesopotamia, present-day Iraq."
+    
+    },
+    {'name': "Temple of Artemis at Ephesus ",
+     'country': "Turkey",
+     'year_built': -650,
+     'description' : "a grand temple in ancient Greece dedicated to the goddess Artemis."
+     
+    },
+    {'name': "Mausoleum at Halicarnassus",
+     'country': "Turkey",
+     'year_built': -353,
+     'description' : " an elaborate tomb built for the Carian king Mausolus and his wife Artemisia."
+     
+    },
+     {'name': "Lighthouse of Alexandria",
+     'country': "Egypty",
+     'year_built': -300,
+     'description' : "a tall lighthouse on the coast of Egypt, built to guide ships into the harbor."
+     
+    },
+     {'name': "Colossus of Rhodes",
+     'country': "Greece",
+     'year_built': -292,
+     'description' : "a massive bronze statue of the Greek god Helios, erected in the city of Rhodes."
+     
+    }
+
+]
