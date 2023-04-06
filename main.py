68a257ae2666c28a545129acb0360cc1f9612569
Villelligiulia <villelligiulia@gmail.com>
2023-04-06 04:04:28 +0000

    Add game introduction and game rules

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ import random
 
 # Display decoration
 print(logo)
+
+print("Welcome to the WonderWorld game!\nDid you know that there are 14 Wonders of the World on our planet?")
+print("7 of them belong to the Modern World and the other 7 belong to the Ancient World")
+print("But could you place them in time?? Let's test Your Knowledge!!")
+print("GAME RULES: Two random wonders will be compared with eachother\nType 'A' or 'B' if you think your choice is the Wonder that was built before the other.")
+print("Example : 'Type 'A' for: Chicken Itza, a complex of Mayan ruins on Mexico's Yucatan Peninsula, in Mexico")
+print("Type 'B' for: The Christ Redeemer, a large statue of Jesus Christ in Rio de Janeiro, Brazil, in Brazil")
+print("The correct answer is 'A' as it was built in 800 AC, while 'B' in 1922. HAVE FUN!!")
+
+
